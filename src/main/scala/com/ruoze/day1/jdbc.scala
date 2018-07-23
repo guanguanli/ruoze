75d6084307c2a3323fd1e9b62b478165953c939f
guanguanli <283184358@qq.com>
2018-07-23 10:32:48 -0600

    11

---
+++ src/main/scala/com/ruoze/day1/jdbc.scala
@@ -0,0 +1,54 @@
+package com.ruoze.day1
+
+import scalikejdbc._
+import scalikejdbc.config._
+
+
+object jdbc {
+
+  case class User(id:Int,name:String,age:Int)
+
+  def main(args: Array[String]): Unit = {
+
+    DB autoCommit { implicit session =>
+      sql"create table user ( id int(20) not null AUTO_INCREMENT, name varchar(10), age INT(3),  primary key (id))".execute.apply()
+    }
+
+    insertBatch("zhangsan",11)
+
+  }
+
+
+
+  //在数据插入的时候建立一个事务。
+  def insertBatch( name: String, age : Int) =
+    DB.localx { implicit session =>
+    SQL("insert into user(name,age) values(?,?)").bind(name, age).update().apply()
+  }
+
+
+
+  def delete(id:Int) = {
+    DB.autoCommit { implicit session =>
+      SQL("delete from user where id = ?").bind(id).update().apply()
+    }
+  }
+
+
+  def update(name:String,age:Int) {
+    DB.autoCommit { implicit session =>
+      SQL("update user set age = ? where name = ?").bind(age, name).update().apply()
+    }
+  }
+
+  //select查询到数据之后会产生一个rs的对象集，然后可以得到这个对象集里面的数据。
+  def select() = {
+    DB.readOnly { implicit session =>
+      SQL("select * from user").map(rs => User(rs.string("name"), rs.int("age"))).list().apply()
+    }
+  }
+
+
+}
+
+
---
+++ src/main/scala/com/ruoze/day3/Writetxt.scala
@@ -0,0 +1,56 @@
+package com.ruoze.day3
+
+import java.io.{File, PrintWriter}
+
+object Writetxt {
+
+
+  def main(args: Array[String]): Unit = {
+
+    var  writer =new PrintWriter(new File("config/log.txt"))
+    for(i <- 1 to 100000)
+      {
+       var contextString= getLineContext
+        writer.println(contextString)
+      }
+    writer.close()
+  }
+
+
+  def getLineContext(): StringBuilder =
+  {
+    var line=new StringBuilder
+
+    //domain
+    var domainarr=List("www.ruozedata.com","www.zhibo8.com","www.dongqiudi.com")
+    var randomNum=(new util.Random()).nextInt(2)
+    var domain=domainarr(randomNum)
+    line.append(domain+"\t")
+
+
+    //trafficd
+    var trafficd=(new util.Random()).nextInt(60000)
+    var sj=(new util.Random()).nextInt(10)
+    if(sj==1)
+      {
+        //var base = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        line.append((new util.Random()).nextString(3) +"\t")
+      }else
+      {
+        line.append(trafficd+"\t")
+      }
+
+
+
+    //time
+    var time1=10+(new util.Random()).nextInt(14)
+    var time2=10+(new util.Random()).nextInt(50)
+
+    line.append("[2018-07-"+time1+" "+time1+":"+time2+":"+time1+"]")
+
+
+    line
+  }
+
+
+}
