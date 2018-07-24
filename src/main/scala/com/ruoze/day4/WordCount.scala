e647e5c20b8d8e4779d9a19d5d5949703f06bf25
guanguanli <283184358@qq.com>
2018-07-24 08:50:26 -0600

    11

--- src/main/scala/com/ruoze/day4/WordCount.scala
+++ src/main/scala/com/ruoze/day4/WordCount.scala
@@ -1,5 +1,14 @@
 package com.ruoze.day4
 
+import scala.io.Source
+
 object WordCount {
+
+  def main(args: Array[String]): Unit = {
+    var log=Source.fromFile("config/log.txt").getLines().toList
+    var logmap=log.flatMap(_.split("\t")).map((_,1)).groupBy(_._1).mapValues(_.map((_._2)).reduce(_+_))
+
+    println(logmap)
+  }
 
 }
